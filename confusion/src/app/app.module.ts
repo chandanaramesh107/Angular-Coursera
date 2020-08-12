64d7b00e8830117922a88bcbad03dea9717f788d
Chandana B R <chandanaramesh107@gmail.com>
2020-08-12 13:06:33 +0530

    Angular Reactive Form part1

--- confusion/src/app/app.module.ts
+++ confusion/src/app/app.module.ts
@@ -10,7 +10,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule} from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -56,7 +60,10 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    MatSelectModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     
   ],
