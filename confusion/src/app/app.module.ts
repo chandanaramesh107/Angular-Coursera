237b8f9d292cb8d1091500988f52d1af5f14ff2b
Chandana B R <chandanaramesh107@gmail.com>
2020-08-14 19:17:17 +0530

    Assignment 3

--- confusion/src/app/app.module.ts
+++ confusion/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule} from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule} from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule} from '@angular/forms';
 
@@ -66,6 +67,7 @@ import { LoginComponent } from './login/login.component';
     MatProgressSpinnerModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSliderModule
 
     
   ],
